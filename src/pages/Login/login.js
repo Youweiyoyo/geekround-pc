055f2b2c278a161ae15d97f9f2ec99d1f470204c
Youwei <yoyoyo9527@163.com>
2022-06-04 16:08:28 +0800

    登录页搭建

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,3 +1,12 @@
+import { Card } from 'antd'
+import './index.scss'
+import logo from '@/assets/img/logo.png'
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+      </Card>
+    </div>
+  )
 }
