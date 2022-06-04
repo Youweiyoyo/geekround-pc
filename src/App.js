e045a1c77428e3d484c117afa35c0bef6d8b9995
Youwei <yoyoyo9527@163.com>
2022-06-04 15:12:34 +0800

    配置路径别名&文件提示

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from '@/pages/Layout/layout'
+import Login from '@/pages/Login/login'
+import { Button } from 'antd'
 function App() {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Button type="primary">Primary Button</Button>
+        <Routes>
+          <Route path="/login" element={<Login />}>
+            login
+          </Route>
+          <Route path="/" element={<Layout />}>
+            loyOut
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
