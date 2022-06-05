7a5118543273dd486d99a0753c7aa21f96c9b3fa
Youwei <yoyoyo9527@163.com>
2022-06-05 17:43:27 +0800

    fix baseUrl -> baseURL

--- craco.config.js
+++ craco.config.js
@@ -9,14 +9,14 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  devServer: {
-    proxy: {
-      '/': {
-        // 这是请求接口中要替换的标识
-        target: 'http://geek.itheima.net/v1_0', // 被替换的目标地址，即把 /api 替换成这个
-        pathRewrite: { '/v1_0': '' }, // 把/api 替换为空
-        changeOrigin: true, // 加了这个属性，那后端收到的请求头中的host是目标地址 target
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       // 这是请求接口中要替换的标识
+  //       target: 'http://geek.itheima.net/v1_0', // 被替换的目标地址，即把 /api 替换成这个
+  //       pathRewrite: { '/v1_0': '' }, // 把/api 替换为空
+  //       changeOrigin: true, // 加了这个属性，那后端收到的请求头中的host是目标地址 target
+  //     },
+  //   },
+  // },
 }
