940bd883633e6292fa9e613cc2912d4966ae960b
Youwei <yoyoyo9527@163.com>
2022-06-05 15:55:13 +0800

    增加子路由

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from '@/pages/Layout/layout'
+import './App.css'
+import LayOut from '@/pages/Layout/layout'
 import Login from '@/pages/Login/login'
+import Home from '@/pages/Home/home'
+import Publish from './pages/Publish/publish'
+import Article from './pages/Article/article'
 import AuthRouter from '@/components/AuthComponent'
-import { Button } from 'antd'
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Button type="primary">Primary Button</Button>
         <Routes>
-          <Route path="/login" element={<Login />}>
-            login
-          </Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route
             path="/"
             element={
               <AuthRouter>
-                <Layout />
+                <LayOut />
               </AuthRouter>
             }
-          ></Route>
+          >
+            <Route index path="home" element={<Home />}></Route>
+            <Route path="article" element={<Article />}></Route>
+            <Route path="publish" element={<Publish />}></Route>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/pages/Layout/layout.js
+++ src/pages/Layout/layout.js
@@ -1,3 +1,57 @@
-export default function Layout() {
-  return <div>Layout</div>
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
+import './layout.scss'
+import { Outlet } from 'react-router-dom'
+const { Header, Sider } = Layout
+export default function LayOut() {
+  const items = [
+    { label: '数据概览', key: 'data' },
+    { label: '内容管理', key: 'content' },
+    { label: '发布文章', key: 'article' },
+  ]
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">user.name</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            items={items}
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            {/* antD 4.20可用，之后的版本报警告 */}
+            {/* <Menu.Item icon={<HomeOutlined />} key="1">
+              数据概览
+            </Menu.Item>
+            <Menu.Item icon={<DiffOutlined />} key="2">
+              内容管理
+            </Menu.Item>
+            <Menu.Item icon={<EditOutlined />} key="3">
+              发布文章
+            </Menu.Item> */}
+          </Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          <Outlet />
+        </Layout>
+      </Layout>
+    </Layout>
+  )
 }
