1b960288880fb5dc003960152ab8d6ad759332c4
Youwei <yoyoyo9527@163.com>
2022-06-07 22:44:21 +0800

    使用Hookg管理数据

--- src/pages/Article/article.js
+++ src/pages/Article/article.js
@@ -1,4 +1,162 @@
+import { Link } from 'react-router-dom'
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+  Table,
+  Space,
+  Tag,
+} from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import 'moment/locale/zh-cn'
+import locale from 'antd/es/date-picker/locale/zh_CN'
+import './index.scss'
+import Http from '@/utils/http'
+import { useState, useEffect } from 'react'
+import img404 from '@/assets/img/error.png'
+
+const { Option } = Select
+const { RangePicker } = DatePicker
+
 const Article = () => {
-  return <div>Article</div>
+  // 频道列表管理
+  const [channelList, setChannelList] = useState([])
+  const loadChannelList = async () => {
+    const { data: res } = await Http.get('/channels')
+    console.log(res, 'res')
+    setChannelList(res.channels)
+  }
+  useEffect(() => {
+    loadChannelList()
+  }, [])
+  const onFinish = (values) => {
+    console.log('[ values ] >', values)
+  }
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      width: 120,
+      render: (cover) => {
+        return <img src={cover || img404} width={80} height={60} alt="" />
+      },
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: (data) => <Tag color="green">审核通过</Tag>,
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate',
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count',
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count',
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count',
+    },
+    {
+      title: '操作',
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        )
+      },
+    },
+  ]
+  const data = [
+    {
+      id: '8218',
+      comment_count: 0,
+      cover: {
+        images: ['http://geek.itheima.net/resources/images/15.jpg'],
+      },
+      like_count: 0,
+      pubdate: '2019-03-11 09:00:00',
+      read_count: 2,
+      status: 2,
+      title: 'wkwebview离线化加载h5资源解决方案',
+    },
+  ]
+  return (
+    <div>
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: -1 }} onFinish={onFinish}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={-1}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="频道" name="channel_id">
+            <Select
+              placeholder="请选择文章频道"
+              initialValues="lucy"
+              style={{ width: 120 }}
+            >
+              {channelList.map((item) => (
+                <Option value={item.id} key={item.id}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="日期" name="date">
+            {/* 传入locale属性 控制中文显示*/}
+            <RangePicker locale={locale}></RangePicker>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 80 }}>
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      <Card title={`根据筛选条件共查询到 count 条结果：`}>
+        <Table rowKey="id" columns={columns} dataSource={data}></Table>
+      </Card>
+    </div>
+  )
 }
+
 export default Article
