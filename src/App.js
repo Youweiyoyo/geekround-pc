bcf18b0c61df5b77f602483a94f00e47ed77c0a5
Youwei <yoyoyo9527@163.com>
2022-06-11 16:58:08 +0800

    懒加载

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import history from '@/utils/history'
+import { lazy, Suspense } from 'react'
 import {
   unstable_HistoryRouter as HistoryRouter,
   Route,
   Routes,
 } from 'react-router-dom'
 import './App.css'
-import LayOut from '@/pages/Layout/layout'
-import Login from '@/pages/Login/login'
-import Home from '@/pages/Home/home'
-import Publish from './pages/Publish/publish'
-import Article from './pages/Article/article'
-import AuthRouter from '@/components/AuthComponent'
+// 路由懒加载
+const LayOut = lazy(() => import('@/pages/Layout/layout'))
+const Publish = lazy(() => import('./pages/Publish/publish'))
+const Article = lazy(() => import('./pages/Article/article'))
+const AuthRouter = lazy(() => import('@/components/AuthComponent'))
+const Login = lazy(() => import('@/pages/Login/login'))
+const Home = lazy(() => import('@/pages/Home/home'))
 
 function App() {
   return (
     <HistoryRouter history={history}>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route
-            path="/"
-            element={
-              <AuthRouter>
-                <LayOut />
-              </AuthRouter>
-            }
-          >
-            <Route index path="home" element={<Home />}></Route>
-            <Route path="article" element={<Article />}></Route>
-            <Route path="publish" element={<Publish />}></Route>
-          </Route>
-        </Routes>
-      </div>
+      <Suspense>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route
+              path="/"
+              element={
+                <AuthRouter>
+                  <LayOut />
+                </AuthRouter>
+              }
+            >
+              <Route index path="home" element={<Home />}></Route>
+              <Route path="article" element={<Article />}></Route>
+              <Route path="publish" element={<Publish />}></Route>
+            </Route>
+          </Routes>
+        </div>
+      </Suspense>
     </HistoryRouter>
   )
 }
